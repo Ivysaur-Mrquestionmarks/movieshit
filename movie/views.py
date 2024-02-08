cffca740b6859cc57fd5dc815add35e524a4d39b
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2024-02-08 11:46:59 -0500

    uwu

--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'name':'Alfonso'})
 def about(request):
-    return HttpResponse('<h1>Nya uwu</h1>')
+    return render(request, 'about.html')
