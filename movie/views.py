f42b0a8925e842f0a308626770494824cd2e1db8
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2024-02-08 11:12:20 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Alfonso'})
 def about(request):
     return HttpResponse('<h1>Nya uwu</h1>')
