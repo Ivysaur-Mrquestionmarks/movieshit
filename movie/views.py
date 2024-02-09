37ec0922568846e6ddb246b9cd3d2755505f4d23
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2024-02-08 19:23:35 -0500

    :)

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
-    return render(request, 'home.html', {'name':'Alfonso'})
+    
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'name':'Alfonso', 'searchTerm':searchTerm, 'movies':movies})
 def about(request):
     return render(request, 'about.html')
