fb71dcf017698123bad5e62ba7c749b2a1252801
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2024-03-01 09:51:25 -0500

    taller dos

---
+++ movie/management/commands/add_movies_db.py
@@ -0,0 +1,23 @@
+
+from django.core.management.base import BaseCommand
+from movie.models import Movie
+import os
+import json
+class Command (BaseCommand):
+    help = 'Load movies from movie_descriptions.json into the Movie model'
+    def handle(self, *args, **kwargs):
+    # Construct the full path to the JSON file
+    
+    
+        json_file_path='movie/management/commands/movies.json'
+        with open(json_file_path, 'r') as file:
+            movies = json.load(file)
+        # Add products to the database                          
+        for i in range(100):
+            movie = movies[i]
+            exist = Movie.objects.filter(title = movie['title']).first() #Se asegura que la película no exista en if not exist:
+            if not exist:
+                Movie.objects.create(title = movie['title'],
+                                    image = 'movie/images/default.jpg', 
+                                    genre = movie['genre'],
+                                    year = movie['year'])
--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
+from django.shortcuts import render
+from .models import Movie  
+from collections import defaultdict
 
 from .models import Movie
 
@@ -7,9 +14,57 @@ def home(request):
     
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
-        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+        movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
     return render(request, 'home.html', {'name':'Alfonso', 'searchTerm':searchTerm, 'movies':movies})
 def about(request):
     return render(request, 'about.html')
+    
+def statistics_view(request):
+    matplotlib.use('Agg')
+    # Obtener todas las películas
+    all_movies = Movie.objects.all()
+    # Crear un diccionario para almacenar la cantidad de películas por año
+    #movie_counts_by_year = {}
+    movie_counts_by_primary_genre = defaultdict(int)  
+    # Filtrar las películas por año y contar la cantidad de películas por año
+    for movie in all_movies:
+        #year = movie.year if movie.year else "None"
+        genres = movie.genre.split(',')
+        if genres:
+            movie_counts_by_primary_genre[genres[0].strip()] += 1
+        else:
+            movie_counts_by_primary_genre['No Genre'] = 1
+    # Ancho de las barras
+    bar_width = 0.5
+    # Posiciones de las barras
+    #bar_positions = range(len(movie_counts_by_year))
+    genres = list(movie_counts_by_primary_genre.keys())
+    counts = list(movie_counts_by_primary_genre.values())
+    bar_positions = range(len(genres))    
+    # Crear la gráfica de barras
+    plt.bar(bar_positions, counts, width=bar_width, align='center')
+    # Personalizar la gráfica
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, genres, rotation=90)
+    # Ajustar el espaciado entre las barras
+    plt.subplots_adjust(bottom=0.3)
+    # Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    # Convertir la gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode('utf-8')
+    # Renderizar la plantilla statistics.html con la gráfica
+    return render(request, 'statistics.html', {'graphic': graphic}) 
+
+def signup(request):
+        email = request.GET.get('email')
+        return render(request,'signup.html',{'email':email})
